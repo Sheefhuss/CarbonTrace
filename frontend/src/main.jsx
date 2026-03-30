67c341ffe71f343643e62439e72f9f4f8a93d766
Sheefhuss <sheefahussain029@gmail.com>
2026-03-30 23:32:38 +0530

    Initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'; 
 import './index.css';
 import App from './App';
 import useAuthStore from './context/authStore';
-
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 useAuthStore.getState().initialize().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
